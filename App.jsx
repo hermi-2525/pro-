be57e86670a65e4f795ca068d98f5b17a5d89cf4
hermi <153022544+hermi-2525@users.noreply.github.com>
2024-12-25 15:45:36 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GroupMembers from './GroupMembers';
+
+const App = () => {
+  return (
+    <div>
+      <GroupMembers />
+    </div>
+  );
+};
+
+export default App;
